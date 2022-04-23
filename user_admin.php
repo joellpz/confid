f51b928572235bda71c8d5069903b5d3bff7ac59
Lomasraro <moisesvalleovejero@gmail.com>
2022-04-23 21:28:10 +0200

    k

--- user_admin.php
+++ user_admin.php
@@ -8,6 +8,9 @@
     <body <?php if($trabajador){echo "class='admin_body' id='body-pd'";}?>>
         <?php include_once 'header.php'; ?>
         <h2>Administración de Usuarios</h2>
-         
+        <a class="btn btn-primary" href="#" role="button">Consultar Usuarios</a>
+        <a class="btn btn-primary" href="#" role="button">Añadir Usuario</a>
+        <a class="btn btn-primary" href="#" role="button">Modificar Usuario</a>
+        <a class="btn btn-primary" href="#" role="button">Eliminar Usuario</a>
     </body>
 </html>
