62f76075aa4c521990637dbe709f4ec000d5dcc9
AngelCM08 <92088141+AngelCM08@users.noreply.github.com>
2022-04-23 20:28:26 +0200

    registro

--- registro.php
+++ registro.php
@@ -1,46 +1,35 @@
 <!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <meta name="author" content="" />
-        <meta name="description" content="Tienda de componentes a los mejores precios. "/>
-        <meta name="keywords" content="pc, ordenador, components, componentes, comprar"/>
-        <meta name="robots" content="noindex"/>
-        <title>COMPOPC | Inicio</title>
-        <link rel="shortcut icon" href="img/logoamedias.png" sizes="64x64"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap" rel="stylesheet">
-        <link rel ="stylesheet" href="styles/inicio.css"/>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <?php 
+        $file=basename(__FILE__, '.php');
+        include 'head.php'; //CAMBIAR RUTA SEGUN UBI
+    ?>  
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    </head>
-    <body>
-        <script>  
-            function write(){
-                var name = document.getElementById('name');
-                var alias = document.getElementById("alias");
+    <script>  
+        function write(){
+            var name = document.getElementById('name');
+            var alias = document.getElementById("alias");
 
-                name.addEventListener("write", )
+            name.addEventListener("write", )
 
-                alias.innerText = 'si';
-            } 
-            name.addEventListener('keypress', write, false);
-            
-            function autonomo(){
-                var check = document.getElementById("auto");
-                var cif = document.getElementById("CIF");
-                if(check.checked){ 
-                    cif.innerHTML = "NIF/DNI*"; 
-                }else{ 
-                    cif.innerHTML = "CIF*";
-                }
+            alias.innerText = 'si';
+        } 
+        name.addEventListener('keypress', write, false);
+        
+        function autonomo(){
+            var check = document.getElementById("auto");
+            var cif = document.getElementById("CIF");
+            if(check.checked){ 
+                cif.innerHTML = "NIF/DNI*"; 
+            }else{ 
+                cif.innerHTML = "CIF*";
             }
-        </script>
+        }
+    </script>
+
+    <body <?php if($trabajador){echo "class='admin_body' id='body-pd'";}?>>
+        <?php include_once 'header.php'; ?> <!-- CAMBIAR RUTA SEGUN UBI Insertar headers segun login -->   
+        
         <form action="php/singup.php" method="post">
             <div class="row" style="height: 150px;"></div>
             <div class="row justify-content-center">
