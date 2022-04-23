eb1c948352d5489a6855c5f762bda442da7c1933
joellpz <joelme001@gmail.com>
2022-04-23 20:27:15 +0200

    a

--- plantilla.php
+++ plantilla.php
@@ -2,10 +2,10 @@
 <html>
     <?php 
         $file=basename(__FILE__, '.php');
-        /* $file= "C. Panel"; */
+        /* $file= "C. Panel"; */ // Panel de Administador SOLO
         include 'head.php'; //CAMBIAR RUTA SEGUN UBI
     ?>  
-    <body <?php if($trabajador){echo "class='admin_body' id='body-pd'";}?>
+    <body <?php if($trabajador){echo "class='admin_body' id='body-pd'";}?>>
         <?php include_once 'header.php'; ?> <!-- CAMBIAR RUTA SEGUN UBI Insertar headers segun login -->
     </body>
 </html>
