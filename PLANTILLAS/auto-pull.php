6f4ec8eb12c992eb2827d2ff19cbeb9b58450adb
joellpz <102882581+joellpz@users.noreply.github.com>
2022-04-28 19:35:38 +0200

    Update auto-pull.php

--- PLANTILLAS/auto-pull.php
+++ PLANTILLAS/auto-pull.php
@@ -1,6 +1,6 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
-  shell_exec( 'cd /srv/www/git-repo/ && git reset --hard HEAD && git pull' );
+  shell_exec( 'cd /var/www/confid/ && git reset --hard HEAD && git pull' );
 }
 ?>hi
