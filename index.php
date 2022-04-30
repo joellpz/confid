04654525a4bc39bf829bf287838e3412ea4522e4
AngelCM08 <92088141+AngelCM08@users.noreply.github.com>
2022-04-30 19:37:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
         <div class="row align-items-center">
             <div class="col">
             <div class="card h-100">
-                <img src="/img/productos/servicio1.jpg" class="card-img-top" alt="...">
+                <img src="/img/productos/servicio1.png" class="card-img-top" alt="...">
                 <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
