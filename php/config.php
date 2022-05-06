4bcf3ea20b928723309001c8b3d63bc627a7fd2f
joellpz <102882581+joellpz@users.noreply.github.com>
2022-05-06 17:34:20 +0200

    Update config.php

--- php/config.php
+++ php/config.php
@@ -3,8 +3,8 @@ $servername = 'localhost';
 $database = 'confid';
 /*$username = 'root';
 $password = '';*/
-$username = 'pi';
-$password = 'raspberry';
+/*$username = 'pi';
+$password = 'raspberry';*/
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 ?>
