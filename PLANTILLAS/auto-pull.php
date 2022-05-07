b2d779e0459917b1d3601cc1c55d80fa84ec74d1
joellpz <102882581+joellpz@users.noreply.github.com>
2022-05-07 14:13:27 +0200

    Update auto-pull.php

--- PLANTILLAS/auto-pull.php
+++ PLANTILLAS/auto-pull.php
@@ -1,8 +1,8 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 //if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
-  shell_exec( 'cd /var/www/confid/');
-  shell_exec( 'git pull > Comprovar.txt');
+  shell_exec( 'cd /var/www/ && rm -r confid/');
+  shell_exec( 'git clone https://github.com/joellpz/confid.git');
   echo shell_exec('cat Comprovar.txt');
 //}
 ?>
