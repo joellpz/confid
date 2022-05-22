83be8bcb384add84f17237d999264e03af685aea
joellpz <joelme001@gmail.com>
2022-05-22 20:43:03 +0200

    a

--- php/consultas/del_Cliente.php
+++ php/consultas/del_Cliente.php
@@ -8,7 +8,7 @@
             $delete = "DELETE FROM usuarios WHERE idUsuario = $id";
             mysqli_query($conn, $delete) or die('Consulta fallida: ' . mysqli_connect_error());
             
-            header("Location:$far/content/CPanel/consulta_usuarios.php"); //Redirigir a Principal.
+            header("Location:$far/content/cpanel.php?cons=users"); //Redirigir a Principal.
         ?>   
         <br>   
     </body>
