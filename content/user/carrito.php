915c495aa2161089e3fd837d318a51b415e6a83e
AngelCM08 <92088141+AngelCM08@users.noreply.github.com>
2022-05-26 15:45:55 +0200

    Update carrito.php

--- content/user/carrito.php
+++ content/user/carrito.php
@@ -19,37 +19,39 @@
         ?>
         <div class="container-fluid my-5">                   
 
-        <?php foreach ($_SESSION['carrito_prod'] as $key => $id_prod) { 
-            
-            $qcateg = 'SELECT * FROM productos WHERE idProductos = '.$id_prod;
-            $rescat = mysqli_query($conn, $qcateg);
-            $row = mysqli_fetch_array($rescat);
-        ?>
-            <div class="row caja_carrito" id="<?php echo $row['idProductos']; ?>">
-                <div class="col-2 img_prod_car">
-                    <img style="width: 190px;" src="/img/productos/<?php echo $row['idProductos'];?>.png" class="card-img-top car_element" alt="...">    
-                </div>
-                <div class="col-2">             
-                    <p class="card-title serv-title car_element"><b><?php echo $row['nombre'];?></b></p>                                                                        
-                </div>  
-                <div class="col-2">             
-                    <p class="card-title serv-title car_element"><b><?php echo $row['stock'];?></b></p>                                                                        
-                </div>  
-                <div class="col-2">             
-                    <p class="card-title serv-title">
-                        <select id="cantidad" name="cantidad" class="form-control car_element">
-                            <?php for($i=1; $i<=$row['stock']; $i++) if($i == $_SESSION['carrito_stock'][$key])echo "<option selected>".$i."</option>"; else echo "<option>".$i."</option>"; ?>
-                        </select>
-                    </p>                                                                        
-                </div>        
-                <div class="col-2">             
-                    <p class="card-title serv-title car_element"><b>PRECIO INDIV</b></p>                                                                        
-                </div>          
-                <div class="col-2">             
-                    <p class="card-title serv-title car_element"><b>PRECIO total</b></p>                                                                        
-                </div>                         
-            </div><br>                      
-        <?php } ?>                             
+        <?php
+        if(isset($_SESSION['carrito_prod'])){
+            foreach ($_SESSION['carrito_prod'] as $key => $id_prod) { 
+                
+                $qcateg = 'SELECT * FROM productos WHERE idProductos = '.$id_prod;
+                $rescat = mysqli_query($conn, $qcateg);
+                $row = mysqli_fetch_array($rescat);
+            ?>
+                <div class="row caja_carrito" id="<?php echo $row['idProductos']; ?>">
+                    <div class="col-2 img_prod_car">
+                        <img style="width: 190px;" src="/img/productos/<?php echo $row['idProductos'];?>.png" class="card-img-top car_element" alt="...">    
+                    </div>
+                    <div class="col-2">             
+                        <p class="card-title serv-title car_element"><b><?php echo $row['nombre'];?></b></p>                                                                        
+                    </div>  
+                    <div class="col-2">             
+                        <p class="card-title serv-title car_element"><b><?php echo $row['stock'];?></b></p>                                                                        
+                    </div>  
+                    <div class="col-2">             
+                        <p class="card-title serv-title">
+                            <select id="cantidad" name="cantidad" class="form-control car_element">
+                                <?php for($i=1; $i<=$row['stock']; $i++) if($i == $_SESSION['carrito_stock'][$key])echo "<option selected>".$i."</option>"; else echo "<option>".$i."</option>"; ?>
+                            </select>
+                        </p>                                                                        
+                    </div>        
+                    <div class="col-2">             
+                        <p class="card-title serv-title car_element"><b>PRECIO INDIV</b></p>                                                                        
+                    </div>          
+                    <div class="col-2">             
+                        <p class="card-title serv-title car_element"><b>PRECIO total</b></p>                                                                        
+                    </div>                         
+                </div><br>                      
+        <?php }} ?>                             
         </div>                      
         
         <?php include_once $far.'php/comun/footer.php'; ?>        
