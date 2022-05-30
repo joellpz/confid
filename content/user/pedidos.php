b84ff5e37ec463ae3b68381063fba275cc3eb90c
joellpz <joelme001@gmail.com>
2022-05-30 23:13:35 +0200

    a

--- content/user/pedidos.php
+++ content/user/pedidos.php
@@ -92,11 +92,6 @@
             </tbody>
         </table>                        
         <!-- DETALLES PEDIDOS -->    
-                    <?php ?>
-
-            
-                                   
-                                    
         </section> 
         <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
