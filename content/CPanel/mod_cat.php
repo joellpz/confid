59057ed68a54372e2663463958fc4e4cd0e71ed0
joellpz <102882581+joellpz@users.noreply.github.com>
2022-06-01 10:23:43 +0200

    Update mod_cat.php

--- content/CPanel/mod_cat.php
+++ content/CPanel/mod_cat.php
@@ -32,8 +32,8 @@
                 mysqli_query($conn, $update_cat) or die('Consulta perico: '. mysqli_error());
                 echo '<script>location.href = "../cpanel.php?cons=prod"</script>';
             }else{
-                echo "Hola";
-                //echo '<script>location.href = "../cpanel.php?cons=prod"</script>';
+                //echo "Hola";
+                echo '<script>location.href = "../cpanel.php?cons=prod"</script>';
             }
             
             include_once $far.'content/header.php'; ?>
